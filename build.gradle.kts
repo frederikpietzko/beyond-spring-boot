520da1ec3f91d3d5eacff9e4a208c735984ee8ac
Frederik Pietzko <frederik.pietzko@iits-consulting.de>
2025-08-10 13:11:18 +0200

    init repo

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+
+plugins {
+
+}
+
+group = "com.github.frederikpietzko"
+version = "1.0.0"
+
+allprojects {
+  group = "com.github.frederikpietzko"
+  version = "1.0.0"
+
+  repositories {
+
+  }
+}
+
